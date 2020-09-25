b4d820120050eaac47a3fd1ec52801c0735e1176
Irrenhauspatient <andreasscheuer85@gmx.de>
2020-09-25 11:49:04 +0200

    .

---
+++ GeometricObjects/Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    class Document : ICopy
+    {
+        public void Copy()
+        {
+            Console.WriteLine($"Das Dokument wird kopiert";
+        }
+        private string _Caption;
+
+        public string Caption
+        {
+            get { return _Caption; }
+            set { _Caption = value; }
+        }
+
+    }
+}
---
+++ GeometricObjects/Interfaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    public interface ICopy
+    {      
+       
+        string Caption { get; set; }
+        void Copy();
+       
+    }
+}
